f3eae69f1b54f5554f78554cf2b6a637b3274520
JURI <80569323+juri123123@users.noreply.github.com>
2024-11-18 14:05:05 +0900

    [ET-46] Filter Expanded View Fix

--- Toucheese/Features/FilterView.swift
+++ Toucheese/Features/FilterView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct FilterView: View {
-
+    
     @State var isChanged: Bool = false
     @State var isHidden: Bool = true
     
@@ -19,80 +19,89 @@ struct FilterView: View {
     @State var selectedPrice: PriceType? = nil
     
     var body: some View {
-        ZStack {
-            VStack {
-                HStack {
-                    if isChanged {
-                        Image(systemName: "arrow.clockwise")
-                            .font(.system(size: 18))
-                            .padding(.trailing, 10)
-                            .onTapGesture {
-                                selectedRegion = nil
-                                selectedRating = nil
-                                selectedPrice = nil
-                                selectedFilterType = nil
-                                isChanged.toggle()
-                                isHidden = true
-                            }
-                    }
-                    ForEach(FilterType.allCases, id: \.self) { filter in
-                        FilterButton(filterName: "\(filter.title)", isSelected: selectedFilterType == filter) {
-                            if isHidden || selectedFilterType == nil {
-                                selectedFilterType = filter
-                                isHidden.toggle()
-                            } else if selectedFilterType != filter {
-                                selectedFilterType = filter
-                            } else {
-                                isHidden = true
-                                selectedFilterType = nil
-                            }
-                        }
-                    }
-                }
-                
-                HStack {
-                    switch selectedFilterType {
-                    case .region:
-                        FilterRadioButton(type: nil, selectedType: $selectedRegion) {
+        VStack {
+            HStack {
+                if isChanged {
+                    Image(systemName: "arrow.clockwise")
+                        .font(.system(size: 18))
+                        .padding(.trailing, 10)
+                        .onTapGesture {
                             selectedRegion = nil
-                        }
-                        ForEach(RegionType.allCases, id:
-                                    \.self) { type in
-                            FilterRadioButton(type: type, selectedType: $selectedRegion) {
-                                isChanged = true
-                            }
-                        }
-                    case .rating:
-                        FilterRadioButton<RatingType>(type: nil, selectedType: $selectedRating) {
                             selectedRating = nil
-                        }
-                        ForEach(RatingType.allCases, id:
-                                    \.self) { type in
-                            FilterRadioButton(type: type, selectedType: $selectedRating) {
-                                isChanged = true
-                            }
-                        }
-                    case .price:
-                        FilterRadioButton<PriceType>(type: nil, selectedType: $selectedPrice) {
                             selectedPrice = nil
+                            selectedFilterType = nil
+                            isChanged.toggle()
+                            isHidden = true
                         }
-                        ForEach(PriceType.allCases, id:
-                                    \.self) { type in
-                            FilterRadioButton(type: type, selectedType: $selectedPrice) {
-                                isChanged = true
-                            }
+                }
+                ForEach(FilterType.allCases, id: \.self) { filter in
+                    FilterButton(filterName: "\(filter.title)", isSelected: selectedFilterType == filter) {
+                        if isHidden || selectedFilterType == nil {
+                            selectedFilterType = filter
+                            isHidden.toggle()
+                        } else if selectedFilterType != filter {
+                            selectedFilterType = filter
+                        } else {
+                            isHidden = true
+                            selectedFilterType = nil
                         }
-                    case nil:
-                        Spacer()
-                            .padding()
                     }
                 }
-                .padding(.vertical, 30)
-                .opacity(isHidden ? 0 : 1)
             }
+            
+            HStack {
+                switch selectedFilterType {
+                case .region:
+                    FilterRadioButton(type: nil, selectedType: $selectedRegion) {
+                        selectedRegion = nil
+                        if selectedPrice == nil && selectedRating == nil && selectedRegion == nil {
+                            isChanged = false
+                        }
+                    }
+                    ForEach(RegionType.allCases, id:
+                                \.self) { type in
+                        FilterRadioButton(type: type, selectedType: $selectedRegion) {
+                            isChanged = true
+                        }
+                    }
+                case .rating:
+                    FilterRadioButton<RatingType>(type: nil, selectedType: $selectedRating) {
+                        selectedRating = nil
+                        if selectedPrice == nil && selectedRating == nil && selectedRegion == nil {
+                            isChanged = false
+                        }
+                    }
+                    ForEach(RatingType.allCases, id:
+                                \.self) { type in
+                        FilterRadioButton(type: type, selectedType: $selectedRating) {
+                            isChanged = true
+                        }
+                    }
+                case .price:
+                    FilterRadioButton<PriceType>(type: nil, selectedType: $selectedPrice) {
+                        selectedPrice = nil
+                        if selectedPrice == nil && selectedRating == nil && selectedRegion == nil {
+                            isChanged = false
+                        }
+                    }
+                    ForEach(PriceType.allCases, id:
+                                \.self) { type in
+                        FilterRadioButton(type: type, selectedType: $selectedPrice) {
+                            isChanged = true
+                        }
+                    }
+                case nil:
+                    Spacer()
+                        .padding()
+                }
+            }
+            .padding(.vertical, 30)
+            .opacity(isHidden ? 0 : 1)
+            .frame(maxWidth: .infinity)
+            .background(isHidden ? .orange.opacity(0) : .yellow)
+            .clipShape(.rect(cornerRadius: 20))
+            .shadow(radius: 5)
         }
-        .frame(maxWidth: .infinity)
-        .background(isHidden ? .orange.opacity(0) : .orange.opacity(0.05))
     }
 }
 
