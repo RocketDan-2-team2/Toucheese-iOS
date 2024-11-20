792101cb57b1d476ddd728609b5f7bc7edb3edeb
JURI <80569323+juri123123@users.noreply.github.com>
2024-11-20 13:10:30 +0900

    chore: enum case 추가

--- Toucheese/Core/Enums/RegionType.swift
+++ Toucheese/Core/Enums/RegionType.swift
@@ -12,12 +12,33 @@ enum RegionType: CaseIterable, FilterProtocol {
     case first
     case second
     case third
+    case fourth
+    case fifth
+    case sixth
+    case seventh
+    case eighth
+    case ninth
     
     var title: String {
         switch self {
-        case .first: "강남/서초/송파"
-        case .second: "강서/마포/영등포"
-        case .third: "강북/용산/성동"
+        case .first:
+            return "강남"
+        case .second:
+            return "서초"
+        case .third:
+            return "송파"
+        case .fourth:
+            return "강서"
+        case .fifth:
+            return "마포"
+        case .sixth:
+            return "영등포"
+        case .seventh:
+            return "강북"
+        case .eighth:
+            return "용산"
+        case .ninth:
+            return "성동"
         }
     }
 }
