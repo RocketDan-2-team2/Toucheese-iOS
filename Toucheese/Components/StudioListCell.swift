b9ce6ee7f58a7d3add533ec054897fbc9601aa10
Jiho <yjh7827@naver.com>
2024-11-22 01:54:03 +0900

    refactor: StudioEntity에 맞춰 변수 수정, systemImage 대신 AsyncImage와 skeleton 적용

--- Toucheese/Components/StudioListCell.swift
+++ Toucheese/Components/StudioListCell.swift
@@ -8,18 +8,13 @@
 import SwiftUI
 
 struct StudioListCell: View {
+    let order: Int
+    let profileImage: String
+    let name: String
+    let popularity: Float
+    let portfolios: [String]
     
-    var order: Int = 0
-    
-    var profileImage: String = "person.fill"
-    
-    var studioLabel: String = "dummy"
-    
-    var rate: Double = 0
-    
-    var portfolioImages: [String] = []
-    
-    @State var isBookmarked: Bool = false
+    @State private var isBookmarked: Bool = false
     
     var body: some View {
         VStack {
@@ -29,13 +24,22 @@ struct StudioListCell: View {
                     .foregroundStyle(.primary)
                     .padding(.horizontal)
                 
-                Image(systemName: profileImage)
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                    .clipShape(.circle)
+                AsyncImage(url: URL(string: profileImage)) { image in
+                    image.resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 50, height: 50)
+                .background(.gray.opacity(0.3))
+                .clipShape(.circle)
+                .overlay {
+                    Circle()
+                        .strokeBorder(.gray)
+                }
                 
                 VStack(alignment: .leading) {
-                    Text(studioLabel)
+                    Text(name)
                         .font(.headline)
                         .foregroundStyle(.primary)
 
@@ -44,7 +48,7 @@ struct StudioListCell: View {
                             .font(.caption)
                             .foregroundStyle(.yellow)
                             
-                        Text("\(rate, specifier: "%.1f")")
+                        Text("\(popularity, specifier: "%.1f")")
                             .font(.caption)
                             .foregroundStyle(.secondary)
                     }
@@ -62,21 +66,33 @@ struct StudioListCell: View {
             }
             
             ScrollView(.horizontal) {
-                HStack {
-                    ForEach(0..<5) { _ in
-                        Image(systemName: "photo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 120, height: 120)
+                LazyHStack {
+                    ForEach(portfolios, id: \.self) { portfolio in
+                        AsyncImage(url: URL(string: portfolio)) { image in
+                            image.resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 147, height: 147)
+                        .background(.gray.opacity(0.3))
+                        .clipShape(.rect(cornerRadius: 20))
                     }
                 }
                 .padding(.horizontal)
             }
+            .scrollIndicators(.hidden)
         }
         .padding(.vertical)
     }
 }
 
 #Preview {
-    StudioListCell()
+    StudioListCell(
+        order: 1,
+        profileImage: "",
+        name: "퓨어&플라워",
+        popularity: 4.8,
+        portfolios: ["a", "b", "c"]
+    )
 }
