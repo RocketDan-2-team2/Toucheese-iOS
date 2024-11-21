bf48cfcf5c2eabd2a967cad1b6e3383736654c92
Jiho <yjh7827@naver.com>
2024-11-21 15:28:43 +0900

    feat: App NavigationStack 적용

--- Toucheese/App/ToucheeseApp.swift
+++ Toucheese/App/ToucheeseApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct ToucheeseApp: App {
     var body: some Scene {
         WindowGroup {
-            ConceptView()
+            NavigationStack {
+                ConceptView()
+            }
         }
     }
 }
