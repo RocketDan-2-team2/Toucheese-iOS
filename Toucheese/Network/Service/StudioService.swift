c9555b7fe4ff2972d1109cbbad2c4f29ed52900d
Jiho <yjh7827@naver.com>
2024-11-26 13:47:21 +0900

    feat: StudioService Default와 Mock 분리

--- Toucheese/Network/Service/StudioService.swift
+++ Toucheese/Network/Service/StudioService.swift
@@ -13,8 +13,8 @@ import Moya
 protocol StudioService {
     func getStudioConceptList() -> AnyPublisher<[ConceptEntity], Error>
     func getStudioList(conceptID: Int,
-                      page: Int,
-                      size: Int) -> AnyPublisher<StudioSearchResultEntity, Error>
+                       page: Int,
+                       size: Int) -> AnyPublisher<StudioSearchResultEntity, Error>
     func searchStudio(conceptID: Int,
                       region: RegionType?,
                       popularity: RatingType?,
@@ -23,7 +23,8 @@ protocol StudioService {
                       size: Int) -> AnyPublisher<StudioSearchResultEntity, Error>
 }
 
-typealias DefaultStudioService = BaseService<StudioAPI>
+
+final class DefaultStudioService: BaseService<StudioAPI> { }
 
 extension DefaultStudioService: StudioService {
     
@@ -38,8 +39,8 @@ extension DefaultStudioService: StudioService {
     ) -> AnyPublisher<StudioSearchResultEntity, any Error> {
         requestObjectWithNetworkError(.search(
             conceptID: conceptID,
-            parameters: [ "page": page,
-                          "size": 10]
+            parameters: ["page": page,
+                         "size": 10]
         ))
     }
     
@@ -57,6 +58,108 @@ extension DefaultStudioService: StudioService {
                          "page": page,
                          "size": 10]
         ))
+    }
+    
+}
+
+
+final class MockStudioService: BaseService<StudioAPI> { }
+    
+extension MockStudioService: StudioService {
+    
+    func getStudioConceptList() -> AnyPublisher<[ConceptEntity], any Error> {
+        return Future { promise in
+            let conceptList: [ConceptEntity] = [
+                .init(id: 1, name: "FLASHY_GLOSSY", image: nil),
+                .init(id: 2, name: "BLACK_BLUE_ACTOR", image: nil),
+                .init(id: 3, name: "NATURAL_PHOTO", image: nil),
+                .init(id: 4, name: "VIBRANT", image: nil),
+                .init(id: 5, name: "WATERCOLOUR_GRAINY", image: nil),
+                .init(id: 6, name: "CLEAR_DOLL_LIKE", image: nil)
+            ]
+            
+            promise(.success(conceptList))
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func getStudioList(
+        conceptID: Int,
+        page: Int,
+        size: Int
+    ) -> AnyPublisher<StudioSearchResultEntity, any Error> {
+        return Future { promise in
+            let studioList: StudioSearchResultEntity = .init(
+                content: [
+                    .init(id: 1, name: "아워유스", profileImage: nil, popularity: 4.8, portfolios: ["a", "b", "c"]),
+                    .init(id: 2, name: "허쉬스튜디오", profileImage: nil, popularity: 4.8, portfolios: ["a", "b", "c", "d"]),
+                    .init(id: 3, name: "레코디드", profileImage: nil, popularity: 4.8, portfolios: ["a", "b"]),
+                    .init(id: 4, name: "셀리온", profileImage: nil, popularity: 4.8, portfolios: ["a", "b", "c", "d"]),
+                    .init(id: 5, name: "리그라피", profileImage: nil, popularity: 4.8, portfolios: ["a", "b", "c", "d"]),
+                ],
+                pageable: .init(
+                    pageNumber: 0,
+                    pageSize: 10,
+                    sort: .init(empty: true, unsorted: true, sorted: false),
+                    offset: 0,
+                    unpaged: false,
+                    paged: true
+                ),
+                last: false,
+                totalPages: 2,
+                totalElements: 19,
+                first: true,
+                size: 10,
+                number: 0,
+                sort: .init(empty: true, unsorted: true, sorted: false),
+                numberOfElements: 10,
+                empty: false
+            )
+            
+            promise(.success(studioList))
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func searchStudio(
+        conceptID: Int,
+        region: RegionType?,
+        popularity: RatingType?,
+        price: PriceType?,
+        page: Int,
+        size: Int
+    ) -> AnyPublisher<StudioSearchResultEntity, any Error> {
+        return Future { promise in
+            let studioList: StudioSearchResultEntity = .init(
+                content: [
+                    .init(id: 1, name: "아워유스", profileImage: nil, popularity: 4.8, portfolios: ["a", "b", "c"]),
+                    .init(id: 2, name: "허쉬스튜디오", profileImage: nil, popularity: 4.8, portfolios: ["a", "b", "c", "d"]),
+                    .init(id: 3, name: "레코디드", profileImage: nil, popularity: 4.8, portfolios: ["a", "b"]),
+                    .init(id: 4, name: "셀리온", profileImage: nil, popularity: 4.8, portfolios: ["a", "b", "c", "d"]),
+                    .init(id: 5, name: "리그라피", profileImage: nil, popularity: 4.8, portfolios: ["a", "b", "c", "d"]),
+                ],
+                pageable: .init(
+                    pageNumber: 0,
+                    pageSize: 10,
+                    sort: .init(empty: true, unsorted: true, sorted: false),
+                    offset: 0,
+                    unpaged: false,
+                    paged: true
+                ),
+                last: false,
+                totalPages: 2,
+                totalElements: 19,
+                first: true,
+                size: 10,
+                number: 0,
+                sort: .init(empty: true, unsorted: true, sorted: false),
+                numberOfElements: 10,
+                empty: false
+            )
+            
+            promise(.success(studioList))
+        }
+        .eraseToAnyPublisher()
     }
     
 }
