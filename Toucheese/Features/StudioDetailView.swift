31aca077a005d2a51142a1c2d9116fe808a31be6
SunJuneL <sunjoon96@gmail.com>
2024-11-26 14:18:44 +0900

    feat: StudioDetailView 파일 추가

---
+++ Toucheese/Features/StudioDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  StudioDetailView.swift
+//  Toucheese
+//
+//  Created by SunJoon Lee on 11/26/24.
+//
+
+import SwiftUI
+
+struct StudioDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudioDetailView()
+}
