6d1510808154a231a60779f156959530c85126dc
Sun Joon Lee <115583150+SunJuneL@users.noreply.github.com>
2024-11-27 15:12:10 +0900

    [ET-83] CarouselView (#18)
    * feat: StudioDetailView 파일 추가
    
    * feat: 스튜디오 상세 페이지에 사용할 CorouselView UI 작업
    
    TODO: 나중에 [스튜디오-리뷰-후기사진] 화면에서도 쓰일 수 있도록 변경 예정
    
    * chores: CorouselView 이름을 StudioCarouselView로 변경

---
+++ Toucheese/Features/StudioDetailView.swift
@@ -0,0 +1,24 @@
+//
+//  StudioDetailView.swift
+//  Toucheese
+//
+//  Created by SunJoon Lee on 11/26/24.
+//
+
+import SwiftUI
+
+struct StudioDetailView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                StudioCarouselView(urls: [""])
+                
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    StudioDetailView()
+}
