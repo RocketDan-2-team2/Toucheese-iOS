f090c49d5a81ace7b0ecaabb97c60a105ea686f6
Jiho <yjh7827@naver.com>
2024-11-27 13:51:26 +0900

    refactor: StudioProductList State 값으로 관리

--- Toucheese/Features/StudioProductListView.swift
+++ Toucheese/Features/StudioProductListView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct StudioProductListView: View {
+    @State private var productList: [StudioProduct] = StudioProduct.mockData
+    
     var body: some View {
         ScrollView {
             StudioNoticeView(notice: "저희 공운스튜디오는 주차장을 따로 운영하고 있습니다! 스튜디오 건물 오른 편으로 돌아 골목에 주차 가능합니다")
@@ -17,16 +19,24 @@ struct StudioProductListView: View {
                 Text("촬영 상품")
                     .font(.headline)
                 
-                ForEach(0...6, id: \.self) { _ in
-                    StudioProductCell()
+                ForEach(productList) { product in
+                    NavigationLink(value: product) {
+                        StudioProductCell(product: product)
+                    }
+                    .buttonStyle(PlainButtonStyle())
                 }
             }
             .padding(.horizontal)
             .padding(.vertical, 8)
         }
+        .navigationDestination(for: StudioProduct.self) { product in
+            Text(product.id)
+        }
     }
 }
 
 #Preview {
-    StudioProductListView()
+    NavigationStack {
+        StudioProductListView()
+    }
 }
