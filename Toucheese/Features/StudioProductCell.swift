3f8be9c12655924fa3cc7aa86684d06b24afcbaf
Jiho <yjh7827@naver.com>
2024-11-27 13:44:53 +0900

    refactor: 기존 필드들 StudioProduct로 묶음

--- Toucheese/Features/StudioProductCell.swift
+++ Toucheese/Features/StudioProductCell.swift
@@ -8,21 +8,10 @@
 import SwiftUI
 
 struct StudioProductCell: View {
-    let name: String
-    let introduction: String
-    let reviewCount: Int
-    let price: Int
+    let product: StudioProduct
     
-    init(
-        name: String = "상품 이름",
-        introduction: String = "상품 소개\n상품 소개\n상품 소개",
-        reviewCount: Int = 1234,
-        price: Int = 123456
-    ) {
-        self.name = name
-        self.introduction = introduction
-        self.reviewCount = reviewCount
-        self.price = price
+    init(product: StudioProduct) {
+        self.product = product
     }
     
     var body: some View {
@@ -33,30 +22,37 @@ struct StudioProductCell: View {
                 .frame(height: 140)
             
             VStack(alignment: .leading, spacing: 6) {
-                Text(name)
+                Text(product.name)
                     .bold()
                     .font(.title3)
                 
-                Text(introduction)
+                Text(product.introduction)
                     .font(.system(size: 14))
                 
                 HStack {
-                    Text("리뷰 \(reviewCount.formatted())개")
+                    Text("리뷰 \(product.reviewCount.formatted())개")
                         .font(.caption)
                         .foregroundStyle(.gray)
                     
                     Spacer()
                     
-                    Text(price.formatted() + "원")
+                    Text(product.price.formatted() + "원")
                         .bold()
                         .font(.title3)
                 }
             }
             .padding(12)
         }
+        .contentShape(.rect)
     }
 }
 
 #Preview {
-    StudioProductCell()
+    StudioProductCell(product: .init(
+        id: UUID().uuidString,
+        name: "상품 이름",
+        introduction: "상품 소개\n상품 소개\n상품 소개",
+        reviewCount: 1234,
+        price: 123456
+    ))
 }
