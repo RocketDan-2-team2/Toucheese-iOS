5c308a055d49536d37e628c0f1d768aecdff8dd5
SunJuneL <sunjoon96@gmail.com>
2024-11-28 14:59:11 +0900

    feature: StudioDetailView에 Sticky Header 적용 및 탭 뷰 작업

--- Toucheese/Features/StudioDetailView.swift
+++ Toucheese/Features/StudioDetailView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct StudioDetailView: View {
     
+    @State private var tabSelection: Int = 0
+    
     let studioEntity: StudioEntity
     let workTime: String
     let location: String
     
-    
     var body: some View {
         ScrollView {
-            VStack {
+            LazyVStack(pinnedViews: .sectionHeaders) {
                 StudioCarouselView(urls: [""])
                     .containerRelativeFrame(.vertical) { length, _ in
                         length * 0.3
@@ -35,22 +36,45 @@ struct StudioDetailView: View {
                     Spacer()
                 }
                 
-                StudioDetailTab()
+                Section {
+                    switch tabSelection {
+                    case 0:
+                        VStack {
+                            ForEach(0..<50, id: \.self) { _ in
+                                Text("가격")
+                            }
+                        }
+                    case 1:
+                        VStack {
+                            ForEach(0..<50, id: \.self) { _ in
+                                Text("리뷰")
+                            }
+                        }
+                    default:
+                        Text("아무것도 없음.")
+                    }
+                } header: {
+                    StudioDetailTab(tabSelection: $tabSelection)
+                }
             }
         }
+        .navigationTitle(studioEntity.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    StudioDetailView(
-        studioEntity: StudioEntity(
-            id: 0,
-            name: "공원스튜디오",
-            profileImage: nil,
-            popularity: nil,
-            portfolios: []
-        ),
-        workTime: "월~금 10:10-19:00 / 매주 월 휴무",
-        location: "서울특별시 서초구 강남대로 11-11"
-    )
+    NavigationStack {
+        StudioDetailView(
+            studioEntity: StudioEntity(
+                id: 0,
+                name: "공원스튜디오",
+                profileImage: nil,
+                popularity: nil,
+                portfolios: []
+            ),
+            workTime: "월~금 10:10-19:00 / 매주 월 휴무",
+            location: "서울특별시 서초구 강남대로 11-11"
+        )
+    }
 }
