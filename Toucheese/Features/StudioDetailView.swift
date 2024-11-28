e500177c3fbd50ff0eb24665297629b3437c2bc6
SunJuneL <sunjoon96@gmail.com>
2024-11-28 13:34:39 +0900

    feat: StudioDetailView에 스튜디오 캐러셀과 스튜디오 상세 정보 컴포넌트 추가

--- Toucheese/Features/StudioDetailView.swift
+++ Toucheese/Features/StudioDetailView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct StudioDetailView: View {
+    
+    let studioEntity: StudioEntity
+    let workTime: String
+    let location: String
+    
+    
     var body: some View {
         VStack {
-            ZStack {
-                StudioCarouselView(urls: [""])
-                
+            StudioCarouselView(urls: [""])
+                .containerRelativeFrame(.vertical) { cgFloat, _ in
+                    cgFloat * 0.3
+                }
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Label(
+                        "\(studioEntity.popularity ?? 0, specifier: "%.1f")",
+                        systemImage: "star"
+                    )
+                    Label(workTime, systemImage: "clock")
+                    Label(location, systemImage: "map")
+                }
+                .padding(20.0)
+                Spacer()
             }
             
+            Spacer()
         }
     }
 }
 
 #Preview {
-    StudioDetailView()
+    StudioDetailView(
+        studioEntity: StudioEntity(
+            id: 0,
+            name: "공원스튜디오",
+            profileImage: nil,
+            popularity: nil,
+            portfolios: []
+        ),
+        workTime: "월~금 10:10-19:00 / 매주 월 휴무",
+        location: "서울특별시 서초구 강남대로 11-11"
+    )
 }
