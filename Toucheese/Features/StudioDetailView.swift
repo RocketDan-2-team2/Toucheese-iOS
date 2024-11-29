fd5eb5923ade69aa461cfb003cb54d77b94dde8a
SunJuneL <sunjoon96@gmail.com>
2024-11-29 13:22:15 +0900

    refactor: StudioDetailView에 가격탭과 리뷰탭 연결. 뷰에 맞게 캐러셀 사진 크기 수정

--- Toucheese/Features/StudioDetailView.swift
+++ Toucheese/Features/StudioDetailView.swift
@@ -12,13 +12,14 @@ struct StudioDetailView: View {
     @State private var tabSelection: Int = 0
     
     let studioEntity: StudioEntity
+    let studioBackgrounds: [String]
     let workTime: String
     let location: String
     
     var body: some View {
         ScrollView {
             LazyVStack(pinnedViews: .sectionHeaders) {
-                StudioCarouselView(urls: [""])
+                StudioCarouselView(urls: studioBackgrounds)
                     .containerRelativeFrame(.vertical) { length, _ in
                         length * 0.3
                     }
@@ -39,27 +40,31 @@ struct StudioDetailView: View {
                 Section {
                     switch tabSelection {
                     case 0:
-                        VStack {
-                            ForEach(0..<50, id: \.self) { _ in
-                                Text("가격")
-                            }
-                        }
+                        StudioProductListView()
                     case 1:
-                        VStack {
-                            ForEach(0..<50, id: \.self) { _ in
-                                Text("리뷰")
-                            }
-                        }
+                        StudioReviewListView(portfolios: studioEntity.portfolios)
                     default:
                         Text("아무것도 없음.")
                     }
                 } header: {
-                    StudioDetailTab(tabSelection: $tabSelection)
+                    VStack {
+                        StudioDetailTab(tabSelection: $tabSelection)
+                    }
+                    .background(
+                        Rectangle()
+                            .fill(.background)
+                    )
                 }
             }
         }
-        .navigationTitle(studioEntity.name)
-        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                ThumbnailNavigationView(
+                    thumbnail: studioEntity.profileImage ?? "",
+                    title: studioEntity.name
+                )
+            }
+        }
     }
 }
 
@@ -69,10 +74,46 @@ struct StudioDetailView: View {
             studioEntity: StudioEntity(
                 id: 0,
                 name: "공원스튜디오",
-                profileImage: nil,
+                profileImage: "https://i.imgur.com/niY3nhv.jpeg",
                 popularity: nil,
-                portfolios: []
+                portfolios: [
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                    "https://i.imgur.com/niY3nhv.jpeg",
+                ]
             ),
+            studioBackgrounds: [
+                "https://i.imgur.com/niY3nhv.jpeg",
+//                "https://i.imgur.com/niY3nhv.jpeg",
+//                "https://i.imgur.com/niY3nhv.jpeg",
+            ],
             workTime: "월~금 10:10-19:00 / 매주 월 휴무",
             location: "서울특별시 서초구 강남대로 11-11"
         )
