ba3ce62e00da124b986f420efd38cba49907fab3
JURI <80569323+juri123123@users.noreply.github.com>
2024-12-06 14:46:33 +0900

    feat: 주문/결제 UI

---
+++ Toucheese/Components/PaymentRadioButton.swift
@@ -0,0 +1,47 @@
+//
+//  PaymentRadioButton.swift
+//  Toucheese
+//
+//  Created by 최주리 on 12/6/24.
+//
+
+import SwiftUI
+
+struct PaymentRadioButton: View {
+    
+    let type: PaymentType
+    @Binding var selectedType: PaymentType
+    
+    let tapAction: (() -> Void)?
+    
+    var isSelected: Bool {
+        selectedType == type
+    }
+    
+    
+    init(
+        type: PaymentType = .PG,
+        selectedType: Binding<PaymentType>,
+        tapAction: (() -> Void)? = nil
+        
+    ) {
+        self.type = type
+        self._selectedType = selectedType
+        self.tapAction = tapAction
+    }
+    
+    var body: some View {
+        Button {
+            selectedType = type
+            tapAction?()
+        } label: {
+            HStack(alignment: .center) {
+                Image(systemName: isSelected ? "circle.fill" : "circle")
+                
+                Text(type.title)
+            }
+            .font(.system(size: 14))
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
