38dfeaec3f337441320d02c8e50d24f9b6b91c22
Jiho Yoo <49361214+jihoooo97@users.noreply.github.com>
2024-12-11 13:30:23 +0900

    Revert "refactor: View depth 개선"
    This reverts commit 945d2c73d06d1dfdf5f89443dea5e911a57c7c4f.

--- Toucheese/Components/ConceptButton.swift
+++ Toucheese/Components/ConceptButton.swift
@@ -12,45 +12,60 @@ struct ConceptButton: View {
     let conceptImage: String
     let conceptName: String
     
-    var tapAction: (() -> Void)?
+    let tapAction: (() -> Void)?
+    
+    init(
+        conceptImage: String,
+        conceptName: String,
+        tapAction: (() -> Void)? = nil
+    ) {
+        self.conceptImage = conceptImage
+        self.conceptName = conceptName
+        self.tapAction = tapAction
+    }
     
     var body: some View {
         Button {
             tapAction?()
         } label: {
-            VStack(spacing: 2) {
-                AsyncImage(url: URL(string: conceptImage)) { phase in
-                    switch phase {
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .scaledToFill()
-                    default:
-                        Color.clear
-                            .skeleton(
-                                with: true,
-                                appearance: .gradient(
-                                    color: Color(uiColor: .lightGray).opacity(0.5),
-                                    background: Color(uiColor: .lightGray).opacity(0.3)
-                                ),
-                                shape: .rectangle
-                            )
+            VStack(spacing: 0) {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.clear)
+                    .aspectRatio(9 / 11, contentMode: .fit)
+                    .overlay {
+                        AsyncImage(url: URL(string: conceptImage)) { phase in
+                            switch phase {
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                            default:
+                                Rectangle()
+                                    .fill(.clear)
+                                    .skeleton(
+                                        with: true,
+                                        appearance: .gradient(
+                                            color: Color(uiColor: .lightGray).opacity(0.5),
+                                            background: .clear
+                                        ),
+                                        shape: .rectangle
+                                    )
+                            }
+                        }
                     }
-                }
-                .aspectRatio(9 / 11, contentMode: .fit)
-                .clipShape(.rect(cornerRadius: 20))
-                .shadow(
-                    color: .black.opacity(0.1),
-                    radius: 8,
-                    y: 2
-                )
+                    .clipShape(.rect(cornerRadius: 20))
+                    .shadow(
+                        color: .black.opacity(0.1),
+                        radius: 8,
+                        y: 2
+                    )
                 
                 Text(conceptName)
                     .font(.system(size: 14, weight: .semibold))
                     .padding(.vertical, 4)
             }
         }
-        .buttonStyle(.plain)
+        .buttonStyle(PlainButtonStyle())
     }
 }
 
