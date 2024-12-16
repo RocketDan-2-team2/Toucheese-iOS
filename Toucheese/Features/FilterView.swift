bd4ad797b6d9da961682e755035a04e8714fde33
SunJuneL <sunjoon96@gmail.com>
2024-12-16 21:00:15 +0900

    style: FilterDrawerView에서 가장 밑 버튼을 제외하고 기본 뼈대 잡기

--- Toucheese/Features/FilterView.swift
+++ Toucheese/Features/FilterView.swift
@@ -29,6 +29,82 @@ struct FilterView: View {
     }
 }
 
+struct FilterDrawerView: View {
+    
+    let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    @Binding var regionSelection: [RegionType]
+    @Binding var ratingSelection: RatingType?
+    @Binding var priceSelection: PriceType?
+    
+    var body: some View {
+        VStack(spacing: 16.0) {
+            HStack {
+                Text("필터")
+                    .font(.system(size: 16.0))
+                    .bold()
+                Spacer()
+                Image(systemName: "xmark")
+                    .bold()
+            }
+            
+            VStack(alignment: .leading) {
+                Text("지역")
+                    .font(.system(size: 14.0))
+                    .padding(.vertical, 11.5)
+                
+                LazyVGrid(columns: columns) {
+                    ForEach(RegionType.allCases, id: \.self) { region in
+                        HStack {
+                            FilterDuplicatedButton(type: region, selectedType: $regionSelection)
+                            Spacer()
+                        }
+                    }
+                }
+            }
+            
+            Divider()
+                .padding(.top, 16.0)
+            
+            VStack(alignment: .leading) {
+                Text("인기")
+                    .font(.system(size: 14.0))
+                    .padding(.vertical, 11.5)
+                
+                ForEach(RatingType.allCases, id: \.self) { rating in
+                    HStack {
+                        FilterRadioButton(type: rating, selectedType: $ratingSelection)
+                        Spacer()
+                    }
+                }
+            }
+            
+            Divider()
+                .padding(.top, 16.0)
+            
+            VStack(alignment: .leading) {
+                Text("가격")
+                    .font(.system(size: 14.0))
+                    .padding(.vertical, 11.5)
+                
+                ForEach(PriceType.allCases, id: \.self) { price in
+                    HStack {
+                        FilterRadioButton(type: price, selectedType: $priceSelection)
+                        Spacer()
+                    }
+                }
+            }
+            
+            Spacer()
+        }
+        .padding(24.0)
+    }
+}
+
 #Preview {
-    StudioListView(concept: .init(id: 1, name: "VIBRANT", image: nil))
+//    StudioListView(concept: .init(id: 1, name: "VIBRANT", image: nil))
+    FilterDrawerView(
+        regionSelection: .constant([]),
+        ratingSelection: .constant(.first),
+        priceSelection: .constant(.first)
+    )
 }
