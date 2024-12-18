dcde709bbd2f69ef6f2c835f46bfc92cd8cb28ea
Jiho <yjh7827@naver.com>
2024-12-18 19:35:01 +0900

    style: Studio 상품 셀 스켈레톤 조정, 리뷰 0개는 갯수 안보이게 변경

--- Toucheese/Features/StudioProductCell.swift
+++ Toucheese/Features/StudioProductCell.swift
@@ -21,55 +21,53 @@ struct StudioProductCell: View {
                 VStack(alignment: .leading, spacing: 0) {
                     Text(product.name)
                         .font(.system(size: 16, weight: .bold))
-                        .padding(.bottom, 8)
                     
                     Text(product.description)
                         .font(.system(size: 14))
+                        .padding(.vertical, 8)
+                 
+                    if product.reviewCount > 0 {
+                        HStack(spacing: 4) {
+                            Image(systemName: "message")
+                            
+                            Text("리뷰 \(product.reviewCount.formatted())개")
+                        }
+                        .font(.system(size: 12))
+                        .foregroundStyle(.gray)
+                        .padding(.bottom, 2)
+                    }
                     
                     Spacer()
                 }
                 
                 Text(product.price.formatted() + "원")
                     .font(.system(size: 18, weight: .bold))
-                
-                HStack(spacing: 4) {
-                    Image(systemName: "message")
-                    
-                    Text("리뷰 \(product.reviewCount.formatted())개")
-                }
-                .font(.system(size: 12))
-                .foregroundStyle(.gray)
-                .padding(.bottom, 2)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.vertical, 5)
             
-            AsyncImage(url: URL(string: product.image)) { phase in
-                switch phase {
-                case .success(let image):
-                    image
-                        .resizable()
-                        .scaledToFill()
-                default:
-                    Rectangle()
-                        .fill(.clear)
-                        .skeleton(
-                            with: true,
-                            appearance: .gradient(
-                                color: Color(uiColor: .lightGray).opacity(0.5),
-                                background: .clear
-                            ),
-                            shape: .rectangle
-                        )
-                }
+            AsyncImage(url: URL(string: product.image)) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+            } placeholder: {
+                Rectangle()
+                    .fill(.clear)
+                    .skeleton(
+                        with: true,
+                        appearance: .gradient(
+                            color: Color(uiColor: .lightGray).opacity(0.2)
+                        ),
+                        shape: .rectangle
+                    )
             }
             .frame(width: 100, height: 140)
-            .clipShape(.rect(cornerRadius: 8))
-            .background {
+            .background(.placeholder)
+            .overlay {
                 RoundedRectangle(cornerRadius: 8)
                     .stroke(.placeholder.opacity(0.3))
-                    .fill(.placeholder.opacity(0.3))
             }
+            .clipShape(.rect(cornerRadius: 8))
         }
         .frame(height: 140)
         .padding()
@@ -88,12 +86,23 @@ struct StudioProductCell: View {
 }
 
 #Preview {
-    StudioProductCell(product: StudioItem(
-        id: 2,
-        name: "응애",
-        description: "응애응애응애",
-        reviewCount: 4,
-        price: 3000,
-        image: ""
-    ))
+    Group {
+        StudioProductCell(product: StudioItem(
+            id: 2,
+            name: "응애",
+            description: "응애응애응애",
+            reviewCount: 4,
+            price: 3000,
+            image: ""
+        ))
+        
+        StudioProductCell(product: StudioItem(
+            id: 2,
+            name: "응애",
+            description: "응애응애응애",
+            reviewCount: 0,
+            price: 3000,
+            image: "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fimg.flayus.com%2Ffiles%2Fattach%2Fimages%2F425025%2F905%2F553%2F106%2Ffee411bdc98d0c301eb145f4ff1cf64a.jpg&type=sc960_832"
+        ))
+    }
 }
