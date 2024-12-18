906b1bb554066b65aa6b93d579f230b9acc906da
SunJuneL <sunjoon96@gmail.com>
2024-12-19 00:14:12 +0900

    style: FilterView의 Drawer부분 UI 작업 (기능 구현 X)

--- Toucheese/Features/FilterView.swift
+++ Toucheese/Features/FilterView.swift
@@ -17,15 +17,31 @@ struct FilterView: View {
     }
     
     var body: some View {
-        VStack {
+        ZStack {
+            VStack {
+                HStack {
+                    FilterButton(buttonType: .representation(hadFiltered: false))
+                    ForEach(FilterType.allCases, id: \.self) { filter in
+                        FilterButton(buttonType: .filterType(title: filter.title))
+                    }
+                    Spacer()
+                }
+                Rectangle()
+            }
+            
             HStack {
-                FilterButton(buttonType: .representation(hadFiltered: false))
-                ForEach(FilterType.allCases, id: \.self) { filter in
-                    FilterButton(buttonType: .filterType(title: filter.title))
+                Spacer()
+                
+                FilterDrawerView(
+                    regionSelection: .constant([]),
+                    ratingSelection: .constant(.first),
+                    priceSelection: .constant(.first)
+                )
+                .containerRelativeFrame(.horizontal, alignment: .trailing) { length, _ in
+                    length * (4 / 5)
                 }
             }
         }
-        .frame(maxWidth: .infinity)
     }
 }
 
@@ -37,74 +53,89 @@ struct FilterDrawerView: View {
     @Binding var priceSelection: PriceType?
     
     var body: some View {
-        VStack(spacing: 16.0) {
-            HStack {
-                Text("필터")
-                    .font(.system(size: 16.0))
-                    .bold()
-                Spacer()
-                Image(systemName: "xmark")
-                    .bold()
-            }
+        ZStack {
+            UnevenRoundedRectangle(topLeadingRadius: 8.0)
+                .fill(.background)
             
-            VStack(alignment: .leading) {
-                Text("지역")
-                    .font(.system(size: 14.0))
-                    .padding(.vertical, 11.5)
+            VStack(spacing: 16.0) {
+                HStack {
+                    Text("필터")
+                        .font(.system(size: 16.0))
+                        .bold()
+                    Spacer()
+                    Image(systemName: "xmark")
+                        .bold()
+                }
                 
-                LazyVGrid(columns: columns) {
-                    ForEach(RegionType.allCases, id: \.self) { region in
+                VStack(alignment: .leading) {
+                    Text("지역")
+                        .font(.system(size: 14.0))
+                        .padding(.vertical, 11.5)
+                    
+                    LazyVGrid(columns: columns, spacing: 16.0) {
+                        ForEach(RegionType.allCases, id: \.self) { region in
+                            HStack {
+                                FilterDuplicatedButton(type: region, selectedType: $regionSelection)
+                                Spacer()
+                            }
+                        }
+                    }
+                }
+                
+                Divider()
+                    .padding(.top, 16.0)
+                
+                VStack(alignment: .leading, spacing: 16.0) {
+                    Text("인기")
+                        .font(.system(size: 14.0))
+                    
+                    ForEach(RatingType.allCases, id: \.self) { rating in
                         HStack {
-                            FilterDuplicatedButton(type: region, selectedType: $regionSelection)
+                            FilterRadioButton(type: rating, selectedType: $ratingSelection)
                             Spacer()
                         }
                     }
                 }
-            }
-            
-            Divider()
-                .padding(.top, 16.0)
-            
-            VStack(alignment: .leading) {
-                Text("인기")
-                    .font(.system(size: 14.0))
-                    .padding(.vertical, 11.5)
                 
-                ForEach(RatingType.allCases, id: \.self) { rating in
-                    HStack {
-                        FilterRadioButton(type: rating, selectedType: $ratingSelection)
-                        Spacer()
+                Divider()
+                    .padding(.top, 16.0)
+                
+                VStack(alignment: .leading, spacing: 16.0) {
+                    Text("가격")
+                        .font(.system(size: 14.0))
+                        .padding(.vertical, 11.5)
+                    
+                    ForEach(PriceType.allCases, id: \.self) { price in
+                        HStack {
+                            FilterRadioButton(type: price, selectedType: $priceSelection)
+                            Spacer()
+                        }
                     }
                 }
-            }
-            
-            Divider()
-                .padding(.top, 16.0)
-            
-            VStack(alignment: .leading) {
-                Text("가격")
-                    .font(.system(size: 14.0))
-                    .padding(.vertical, 11.5)
                 
-                ForEach(PriceType.allCases, id: \.self) { price in
-                    HStack {
-                        FilterRadioButton(type: price, selectedType: $priceSelection)
-                        Spacer()
-                    }
+                Spacer()
+                
+                HStack(spacing: 8.0) {
+                    Text("초기화")
+                        .font(.system(size: 14.0))
+                        .bold()
+                        .toucheeseButtonStyle(
+                            style: .medium,
+                            padding: (vertical: 11.5, horizontal: 21.5)
+                        )
+                    Text("n개의 스튜디오 보기")
+                        .font(.system(size: 14.0))
+                        .bold()
+                        .toucheeseButtonStyle(
+                            shapeStyle: .fullWidth
+                        )
                 }
             }
-            
-            Spacer()
+            .padding([.top, .horizontal], 24.0)
         }
-        .padding(24.0)
     }
 }
 
 #Preview {
-//    StudioListView(concept: .init(id: 1, name: "VIBRANT", image: nil))
-    FilterDrawerView(
-        regionSelection: .constant([]),
-        ratingSelection: .constant(.first),
-        priceSelection: .constant(.first)
-    )
+    FilterView(studioViewModel: StudioViewModel(), selectedFilterType: .constant(.price))
 }
