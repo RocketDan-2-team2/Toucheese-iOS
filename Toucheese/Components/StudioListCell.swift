5dbdf8f2557f7a118131feb2711851beb11d4b6b
Jiho <yjh7827@naver.com>
2024-12-18 19:00:31 +0900

    style: StudioCell 디자인 적용, 스켈레톤 처리

--- Toucheese/Components/StudioListCell.swift
+++ Toucheese/Components/StudioListCell.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SkeletonUI
 
 struct StudioListCell: View {
     let order: Int
@@ -14,80 +15,80 @@ struct StudioListCell: View {
     let popularity: Float
     let portfolios: [String]
     
-    @State private var isBookmarked: Bool = false
-    
     var body: some View {
-        VStack {
+        VStack(spacing: 16) {
             HStack {
-                Text("\(order)")
-                    .font(.caption)
-                    .foregroundStyle(.primary)
-                    .padding(.horizontal)
-                
                 AsyncImage(url: URL(string: profileImage)) { image in
                     image.resizable()
                         .scaledToFit()
+                        .clipShape(.circle)
                 } placeholder: {
-                    ProgressView()
-                }
-                .frame(width: 50, height: 50)
-                .background(.gray.opacity(0.3))
-                .clipShape(.circle)
-                .overlay {
                     Circle()
-                        .strokeBorder(.gray)
+                        .fill(.clear)
+                        .skeleton(
+                            with: true,
+                            appearance: .gradient(
+                                color: Color(uiColor: .lightGray).opacity(0.2)
+                            ),
+                            shape: .circle
+                        )
+                }
+                .frame(width: 32, height: 32)
+                .background {
+                    Circle()
+                        .fill(.placeholder)
                 }
                 
-                VStack(alignment: .leading) {
-                    Text(name)
-                        .font(.headline)
-                        .foregroundStyle(.primary)
-
-                    HStack {
-                        Image(systemName: "star.fill")
-                            .font(.caption)
-                            .foregroundStyle(.yellow)
-                            
-                        Text("\(popularity, specifier: "%.1f")")
-                            .font(.caption)
-                            .foregroundStyle(.secondary)
-                    }
-                }
+                Text(name)
+                    .font(.system(size: 16, weight: .semibold))
+                    .foregroundStyle(.primary)
+                
                 Spacer()
                 
-                Button {
-                    isBookmarked.toggle()
-                } label: {
-                    Label("Bookmark", systemImage: isBookmarked ? "bookmark.fill" : "bookmark")
-                        .labelStyle(.iconOnly)
-                        .foregroundStyle(.primary)
+                HStack(spacing: 4) {
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(.yellow)
+                        
+                    Text("\(popularity, specifier: "%.1f")")
+                        .foregroundStyle(.gray)
                 }
-                .padding(.horizontal)
+                .font(.system(size: 12, weight: .medium))
             }
             
             ScrollView(.horizontal) {
-                LazyHStack {
+                LazyHStack(spacing: 6) {
                     ForEach(portfolios, id: \.self) { portfolio in
                         AsyncImage(url: URL(string: portfolio)) { image in
                             image.resizable()
                                 .scaledToFill()
                         } placeholder: {
-                            ProgressView()
+                            Rectangle()
+                                .fill(.clear)
+                                .skeleton(
+                                    with: true,
+                                    appearance: .gradient(
+                                        color: Color(uiColor: .lightGray).opacity(0.2)
+                                    ),
+                                    shape: .rectangle
+                                )
                         }
-                        .frame(width: 147, height: 147)
-                        .background(.gray.opacity(0.3))
+                        .frame(width: 100, height: 140)
+                        .background(.placeholder)
                         .clipShape(.rect(cornerRadius: 20))
                     }
                 }
-                .padding(.horizontal)
             }
             .scrollIndicators(.hidden)
         }
+        .padding(12)
         .background {
-            Rectangle()
-                .fill(.background)
+            RoundedRectangle(cornerRadius: 24)
+                .fill(.white)
+                .shadow(
+                    color: .black.opacity(0.08),
+                    radius: 8
+                )
         }
-        .padding(.vertical)
     }
 }
 
@@ -97,6 +98,12 @@ struct StudioListCell: View {
         profileImage: "",
         name: "퓨어&플라워",
         popularity: 4.8,
-        portfolios: ["a", "b", "c"]
+        portfolios:[
+            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA4MjFfNyAg%2FMDAxNzI0MTc4NDkxMjEw.yqR04Au7pctHZpPdFmoXxjmWvGdljNdkLDpLD4TxlRgg.D5bXtwnGoyY8Llc93kRlCTga0KqWRlaK3xu_WkBpU4cg.JPEG%2FIMG_1965.JPG&type=sc960_832",
+            "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fimg.flayus.com%2Ffiles%2Fattach%2Fimages%2F425025%2F905%2F553%2F106%2Ffee411bdc98d0c301eb145f4ff1cf64a.jpg&type=sc960_832",
+            "https://postfiles.pstatic.net/MjAyNDAxMTFfMTAw/MDAxNzA0OTAyMzM0Njcy.BtsOvTb73MR5ekJNKs8Enaz1kAYdvR8X8rOoPGFJjvwg.92FqJLcknfrD576kp3pKpexCoJO4Xh8SWEbaLENARaIg.JPEG.godzilla1998/GBJyQ8ibAAAlgc5.jpg?type=w773",
+            "d"
+        ]
     )
+    .frame(width: 361, height: 220)
 }
